a4dd5fb101828f7a9f3a805dfc60ca7b8d4e6151
Amiel Nava <amieln@icloud.com>
2023-09-18 19:08:42 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // Send build static files to our server to serve.
-  build: {
-    outDir: "../ReactRoastDotnet.API/wwwroot",
-    emptyOutDir: true,
-  },
+    plugins: [react()],
+    // Send build static files to our server to serve.
+    build: {
+    },
 })
